71c3da6a34c90bbc7da6f5581103cdb429d830a7
Remko Duursma <remkoduursma@gmail.com>
2015-10-08 07:53:33 +1100

    Once again only one curve for tumspot

--- R/analysis.R
+++ R/analysis.R
@@ -17,8 +17,7 @@ tumspot$Season <- factor(format(tumspot$Date, "%m-%Y"))
 
 # Choose only one season (the one with the best fit, of course)
 # tumspot <- subset(tumspot, Season %in% c("11-2001","05-2002"))
-tumspot_1 <- subset(tumspot, Season == "11-2001")
-tumspot_2 <- subset(tumspot, Season == "05-2002")
+tumspot <- subset(tumspot, Season %in% c("11-2001","05-2002"))
 
 # Fit A-Ci curves for Figure 5.
 # poor curves (no saturation)
@@ -33,25 +32,14 @@ if(!file.exists(cf)){
 }
 
 # Fit BB model to spot gas exchange data for Figure 5.
-gfit_1 <- fitBB(tumspot_1, gsmodel="BBOpti")
-gfit_2 <- fitBB(tumspot_2, gsmodel="BBOpti")
+gfit <- fitBB(tumspot, gsmodel="BBOpti")
+tumspot$GSpred <- predict(gfit$fit, tumspot)
+g1 <- coef(gfit)[[2]]
+g1ci <- suppressMessages(unname(confint(gfit$fit)))
 
-tumspot_1$GSpred <- predict(gfit_1$fit, tumspot_1)
-tumspot_2$GSpred <- predict(gfit_2$fit, tumspot_2)
-
-g1_1 <- coef(gfit_1)[[2]]
-g1ci_1 <- suppressMessages(unname(confint(gfit_1$fit)))
-
-g1_2 <- coef(gfit_2)[[2]]
-g1ci_2 <- suppressMessages(unname(confint(gfit_2$fit)))
-
-
-lmgpred_1 <- lm(Cond ~ GSpred, data=tumspot_1)
-lmgpred_2 <- lm(Cond ~ GSpred, data=tumspot_2)
-lmgpredt_1 <- tidy(lmgpred_1)
-lmgpredt_2 <- tidy(lmgpred_2)
-lmgpredg_1 <- glance(lmgpred_1)
-lmgpredg_2 <- glance(lmgpred_2)
+lmgpred <- lm(Cond ~ GSpred, data=tumspot)
+lmgpredt <- tidy(lmgpred)
+lmgpredg <- glance(lmgpred)
 
 # Regression of Jmax vs. Vcmax (used in caption for Figure 5).
 p <- coef(acifits)
